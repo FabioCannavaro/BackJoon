056778005a30385990b28371755f0f0a4c008b57
FabioCannavaro <113659510+FabioCannavaro@users.noreply.github.com>
2023-07-04 15:18:20 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,10 @@
+while True:
+    num=list(map(int, input().split()))
+    if (num[0]==0 and num[1]==0 and num[2]==0):
+        break
+    else:
+        num.sort()
+        if ((num[0]*num[0])+(num[1]*num[1]))==(num[2]*num[2]):
+            print("right")
+        else:
+            print("wrong")
