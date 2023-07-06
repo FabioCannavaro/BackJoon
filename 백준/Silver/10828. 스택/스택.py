6cb360b9bc0abbdcccdee08525a22d77b7cd7881
FabioCannavaro <113659510+FabioCannavaro@users.noreply.github.com>
2023-07-06 14:06:47 +0900

    [Silver IV] Title: 스택, Time: 72 ms, Memory: 34140 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+n = int(sys.stdin.readline())
+queue = deque()
+
+for i in range(n):
+    action = sys.stdin.readline().split()
+
+    if action[0] == 'push':
+        queue.append(int(action[1]))
+    elif action[0] == 'pop':
+        if queue:
+            print(queue.pop())
+        else:
+            print(-1)
+    elif action[0] == 'size':
+        print(len(queue))
+    elif action[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif action[0] == 'top':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
+
