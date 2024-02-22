632c56161c96c450401f69b360f0cdd0262ef020
FabioCannavaro <113659510+FabioCannavaro@users.noreply.github.com>
2024-02-23 00:20:26 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 45892 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,10 @@
+city=int(input())
+distance=list(map(int, input().split()))
+price=list(map(int, input().split()))
+total_price=0
+min_price=price[0]
+for i in range(city-1):
+    if price[i]<min_price:
+        min_price=price[i]
+    total_price+=min_price * distance[i]
+print(total_price)
